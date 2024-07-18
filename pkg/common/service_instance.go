396c6e0fdb649154edd99c2617d50971cc80163e
OrangeDou <wenqiangsspu@163.com>
2024-07-18 13:39:41 +0800

    modify：注释

--- pkg/common/service_instance.go
+++ pkg/common/service_instance.go
@@ -4,8 +4,8 @@ package common
 type ServiceInstance struct {
 	Host          string
 	Port          int
-	Weight        int //服务权重
-	CurrentWeight int
+	Weight        int //服务权重，固定不变
+	CurrentWeight int //当前权重，动态调整（服务降级）
 
 	GrpcPort int
 }
